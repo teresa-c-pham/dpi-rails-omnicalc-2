2f888ead2e476ad104dc2fda7d713fe9c9c2b619
Teresa Pham <79329784+teresa-c-pham@users.noreply.github.com>
2024-11-27 23:10:27 +0000

    Fix errors in add, subtract, divide, and multiply

--- app/controllers/addition_controller.rb
+++ app/controllers/addition_controller.rb
@@ -4,12 +4,12 @@ class AdditionController < ApplicationController
   end
 
   def add_these
-    @first_number = params.fetch("first_number").to_f
-    @second_number = params.fetch("second_num").to_f
+    @first_num = params.fetch("first_num").to_f
+    @second_num = params.fetch("second_num").to_f
 
-    @result = @first_number + @second_number
+    @result = @first_num + @second_num
 
 
-    render({ :template => "addition_template/add_results" })
+    render({ :template => "addition_templates/add_results" })
   end
 end
