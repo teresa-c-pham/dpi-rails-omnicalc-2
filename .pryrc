50e0a575be0e7733f7f304131318e6a626bcd9e4
Teresa Pham <79329784+teresa-c-pham@users.noreply.github.com>
2024-12-06 05:27:00 +0000

    Adjust style/string literals for addition controller and config files

--- .pryrc
+++ .pryrc
@@ -1,7 +1,8 @@
+# frozen_string_literal: true
 Pry.config.print = proc do |output, value, _pry_|
   case value
   when ActiveRecord::Relation
-    output.puts "=> #{value.to_s}"
+    output.puts "=> #{value}"
   else
     Pry::ColorPrinter.default(output, value, _pry_)
   end
--- app/controllers/addition_controller.rb
+++ app/controllers/addition_controller.rb
@@ -1,15 +1,17 @@
+# frozen_string_literal: true
+# Controller for Addition forms and templates
 class AdditionController < ApplicationController
   def show_addition_form
-    render({ :template => "addition_templates/add_form" })
+    render({ template: 'addition_templates/add_form' })
   end
 
   def add_these
-    @first_num = params.fetch("first_num").to_f
-    @second_num = params.fetch("second_num").to_f
+    @first_num = params.fetch('first_num').to_f
+    @second_num = params.fetch('second_num').to_f
 
     @result = @first_num + @second_num
 
 
-    render({ :template => "addition_templates/add_results" })
+    render({ template: 'addition_templates/add_results' })
   end
 end
